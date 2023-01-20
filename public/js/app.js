057916709c43003578cd4c27c97386e65c1574be
Vlad Dovnar <dovnarvladislav@gmail.com>
2023-01-20 18:38:34 +0200

    App is prepared for hiroku.

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ let message2 = document.querySelector('#m2');
 weatherForm.addEventListener('submit', (event)=>{
     event.preventDefault();
     console.log(search.value);
-    fetch('http://localhost:3000/weather?address=' + search.value).then((response) => {
+    fetch('/weather?address=' + search.value).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 message1.innerText = data.error;
